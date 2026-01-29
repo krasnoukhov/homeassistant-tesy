772589becfcab1d5d3655af6c238a5402ebf316c
Alex Kolarski <aleks.rk@gmail.com>
2026-01-29 05:08:52 +0200

    Added Mode Auto

--- custom_components/tesy/tesy_oldapi.py
+++ custom_components/tesy/tesy_oldapi.py
@@ -94,6 +94,7 @@ class TesyOldApi:
             mapping = {
                 "manual": "0",
                 "p1": "1",
+                "auto": "1",
                 "p2": "2",
                 "p3": "3",
                 "eco": "4",
